72559ce974b34d24a469fdeba01b8cc662866ef6
Nikita Lopanov <n.lopanov@allfuneral.com>
2024-09-08 20:15:07 +0300

    constants added

---
+++ constants/app-common.ts
@@ -0,0 +1,3 @@
+export const MOCK_USERNAME = 'User';
+export const AVATAR_PLACEHOLDER_SRC =
+  'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541';
