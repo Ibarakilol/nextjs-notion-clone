8a1c48451530dcadae9cd373b21cc634380a2c5c
Nikita Lopanov <n.lopanov@allfuneral.com>
2024-09-08 21:54:02 +0300

    global app components added

---
+++ app/error.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+import { AppRoute } from '@/constants';
+
+const Error = () => {
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image alt="Error" height={300} src="/error.png" width={300} />
+      <h2 className="text-xl font-medium">Something went wrong!</h2>
+      <Button asChild>
+        <Link href={AppRoute.DOCUMENTS}>Go back</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default Error;
