5694ea0dd55ab4fb8a0cc1a71980d2970577f1c2
Nikita Lopanov <n.lopanov@allfuneral.com>
2024-09-16 23:42:47 +0300

    eslint rules updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config = {
   darkMode: ['class'],
-  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './pages/**/*.{ts,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   prefix: '',
   theme: {
     container: {
